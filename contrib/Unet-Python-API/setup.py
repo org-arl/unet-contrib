f7fdf8efeb4b7ca0573d2ba957012b40d9629d38
Prasad Anjangi <prasadtiru@gmail.com>
2018-05-24 11:00:57 +0800

    Added fjagepy version requirement in dependencies.

--- contrib/Unet-Python-API/setup.py
+++ contrib/Unet-Python-API/setup.py
@@ -5,12 +5,12 @@ with open('README.rst') as f:
 
 setup(
     name='unetpy',
-    version='1.3b2',
+    version='1.3b3',
     description='Unet Python Gateway',
     long_description=readme,
     author='Mandar Chitre, Prasad Anjangi',
-    author_email='mandar@subnero.com, prasad@subnero.com',
-    url='https://github.com/org-arl/unet-contrib/tree/master/contrib/Unet-Python-API/unetpy',
+    author_email='mandar@arl.nus.edu.sg, prasad@subnero.com',
+    url='https://github.com/org-arl/unet-contrib/tree/master/contrib/Unet-Python-API',
     license='BSD (3-clause)',
     python_requires='>=3',
     classifiers=[
@@ -19,5 +19,5 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=['fjagepy']
+    install_requires=['fjagepy>=1.4.2b2']
 )
