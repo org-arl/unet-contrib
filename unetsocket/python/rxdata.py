55987183f219bd9537fa227a34bfb48019e263c7
Chinmay Pendharkar <notthetup@gmail.com>
2020-08-13 15:59:50 +0800

    feat(unetsocket): added rxdata/txdata samples for python

---
+++ unetsocket/python/rxdata.py
@@ -0,0 +1,52 @@
+#///////////////////////////////////////////////////////////////////////////////
+#//
+#// Script to transmit data.
+#//
+#// In terminal window (an example):
+#//
+#// $ rxdata.py <ip_address> [port]
+#//
+#////////////////////////////////////////////////////////////////////////////////
+
+from unetpy import *
+import sys
+
+port = 1100
+ip_address = 'localhost'
+
+if (len(sys.argv) < 2):
+	print("Usage : rxdata <ip_address> <rx_node_address> <port> \n"
+	      "ip_address: IP address of the transmitter modem. \n"
+	      "port: port number of transmitter modem. \n"
+	      "A usage example: \n"
+	      "rxdata.py 192.168.1.20 1100\n");
+	sys.exit();
+
+ip_address = sys.argv[1]
+
+if (len(sys.argv) > 2):
+	port = int(sys.argv[2])
+
+sock = UnetSocket(ip_address, port)
+if ( sock == None ):
+	print("Couldn't open unet socket");
+	sys.exit();
+
+# Bind to protocol DATA
+sock.bind(Protocol.DATA);
+
+# Set a timeout of 10 seconds
+sock.setTimeout(10000);
+
+# Receive and display data
+ntf = sock.receive()
+
+if (ntf != None):
+	print(ntf)
+else:
+	print("Error receiving data")
+
+# Close the unet socket
+sock.close()
+
+print("Reception Complete");
