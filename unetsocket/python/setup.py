9e452696aa465a6cbb6665fedff69b9fd6b493a0
Prasad Anjangi <prasadtiru@gmail.com>
2020-10-09 15:27:54 +0800

    chore(unetpy): updated the unetpy version number for release

--- unetsocket/python/setup.py
+++ unetsocket/python/setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='unetpy',
-    version='3.1.0',
+    version='3.1.1',
     description='Unet Python Gateway',
     long_description=readme,
     author='Mandar Chitre, Prasad Anjangi',
