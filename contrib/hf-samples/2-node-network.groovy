0a8c968a710f12675b27999c2180f123790f50fa
prasadtiru <prasad.anjangi@nus.edu.sg>
2021-05-12 11:29:36 +0800

    Update contrib/hf-samples/2-node-network.groovy
    Co-authored-by: Manu Ignatius <manuignatius@users.noreply.github.com>

--- contrib/hf-samples/2-node-network.groovy
+++ contrib/hf-samples/2-node-network.groovy
@@ -35,5 +35,5 @@ modem.dataRate =        [10000.bps, 15000.bps]
 // run the simulation forever
 simulate {
   node 'A', location: [ 0.km, 0.km, -8.m], web: 8081, api: 1101, stack: "$home/etc/setup"
-  node 'B', location: [ 1.km, 0.km, -15.m], web: 8082, api: 1102, stack: "$home/etc/setup"
+  node 'B', location: [ 0.2.km, 0.km, -5.m], web: 8082, api: 1102, stack: "$home/etc/setup"
 }
