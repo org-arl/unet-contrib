ef6c23aaeec25235d041c7f8c0d23ffc4d585faa
Chinmay Pendharkar <notthetup@gmail.com>
2022-06-10 11:17:35 +0800

    fix(unetj): return null when no agent are found

--- unetsocket/js/src/unetutils.js
+++ unetsocket/js/src/unetutils.js
@@ -386,6 +386,7 @@ class CachingGateway extends Gateway{
    */
   async agentForService(service, caching=true, greedy=true) {
     const aid = await super.agentForService(service);
+    if (!aid) return aid;
     return caching ? new CachingAgentID(aid, null, null, greedy) : aid;
   }
 
